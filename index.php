9454bc5ba342ebd6fa3ac2b684ec129829dc9779
bleau lucas <lulubleau39@gmail.com>
2023-09-12 13:11:15 +0200

    1

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang=en>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Portfolio - Lucas BLEAU</title>
+</head>
+<body>
+
+<div class="page">
+    <div class="slide">
+        <h1>Lucas BLEAU</h1>
+    </div>
+
+    <div class="slide">
+        <h1>bloc 2</h1>
+    </div>
+    <div class="slide">
+        <h1>bloc 3</h1>
+    </div>
+    <div class="slide">
+        <h1>bloc 4</h1>
+    </div>
+    <div class="slide">
+        <h1>bloc 5</h1>
+
+        <div class="main">
+            <div class="d1"></div>
+            <div class="d2"></div>
+            <div class="d3"></div>
+            <div class="d4"></div>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
